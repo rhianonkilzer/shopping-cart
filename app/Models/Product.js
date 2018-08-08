e93a68226cfbbfe77612417310a73855b547c1af
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-08-08 16:42:02 -0600

    commit

---
+++ app/Models/Product.js
@@ -0,0 +1,10 @@
+
+
+export default class Product{
+    constructor(name, quantity, price, color){
+        this.name = name
+        this.quantity = quantity
+        this.price = price
+        this.color = color
+    }
+}
